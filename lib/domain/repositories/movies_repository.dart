80cf3f30d0876d5663fb113baa47cbf97906ef32
TonyNina <119764426+TonyNina@users.noreply.github.com>
2023-11-08 15:06:28 -0500

    Fisrt commit

---
+++ lib/domain/repositories/movies_repository.dart
@@ -0,0 +1,7 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+abstract class Movierepoxitory {
+
+//info de retorno una lista de movie
+  Future<List<Movie>> getNowPlaying( { int page = 1} );
+}
